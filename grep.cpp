4c95b189651fb87a500bd3d33f7b5f6a721fa276
Robert Fisher <robertfi@usau-robertfi-m.attlocal.net>
2023-07-25 23:28:59 -0500

    Improvements to grep.cpp

--- grep.cpp
+++ grep.cpp
@@ -1,18 +1,24 @@
 #include "libx.hpp"
 #include <algorithm>
+#include <cstdlib>
 #include <format>
 #include <fstream>
 #include <iostream>
-#include <iterator>
 #include <ranges>
 #include <regex>
 #include <string>
 #include <string_view>
 #include <vector>
 
+///////////////////////////////////////////////////////////////////////////////
+// Namespace abbreviations:
+
 namespace s = std;
 namespace r = std::ranges;
 namespace v = std::views;
+
+///////////////////////////////////////////////////////////////////////////////
+// Line makes it easy to read lines from an istream.
 
 struct Line {
     std::string text;
@@ -30,37 +36,52 @@ std::istream& operator>>(std::istream& in, Line& line)
     return in;
 }
 
+///////////////////////////////////////////////////////////////////////////////
+
 int main(const int argc, const char** argv)
 {
+    // Turn argv into a vector:
     s::vector<s::string_view> args(argv + 1, argv + argc);
 
-    s::regex pattern{"."};
-    if (args.size() > 0) {
-        pattern = s::regex(begin(args[0]), end(args[0]));
+    if (args.empty()) {
+        x::println(s::cerr, "Usage: {} pattern [file]", argv[0]);
+        return EXIT_FAILURE;
     }
 
+    // Create regex:
+    // Can't construct regex from a string_view, so use iterators.
+    s::regex pattern = s::regex(begin(args[0]), end(args[0]));
+
+    // If a file name is given, open it; otherwise, use stdin.
     s::ifstream fin;
     if (args.size() > 1) {
+        // Can't pass a string_view, so convert it to a string.
         fin.open(s::string{args[1]});
     }
     s::istream& in = fin.is_open()? fin: s::cin;
 
+    // A helper function for the filter view below.
     auto match = [&pattern](const auto& tuple)
     {
         const auto& [number, line] = tuple;
-        return s::regex_search(line.text, pattern);
+        return s::regex_search(line, pattern);
     };
 
+    // Turn an <int, std::string> tuple-like object into a string:
     auto stringify = [](const auto& tuple)
     {
         const auto& [number, line] = tuple;
-        return s::format("{}: {}", number + 1, line.text);
+        return s::format("{}: {}", number + 1, line);
     };
 
-    r::copy(r::istream_view<Line>(in)
-            | v::enumerate
-            | v::filter(match)
-            | v::transform(stringify),
+    // Do the work and write it to stdout:
+    r::copy(r::istream_view<Line>(in)   // Read lines
+            | v::transform(&Line::text) // Turn line objects into strings
+            | v::enumerate              // Number the lines
+            | v::filter(match)          // Do the regex match
+            | v::transform(stringify),  // Format the output
             s::ostream_iterator<s::string>(s::cout, "\n"));
+
+    return EXIT_SUCCESS;
 }
 
