df8479ff66625bfcf128bdc3bcc3bfadf1a313f4
Robert Fisher <robertfi@usau-robertfi-m.attlocal.net>
2023-07-26 00:09:27 -0500

    Updates to problem.cpp
    The problem.cpp program illustrates how lines of characters created by
    lazy_split disappear when sent through filter.

--- problem.cpp
+++ problem.cpp
@@ -7,22 +7,6 @@
 #include <sstream>
 #include <format>
 
-struct Line {
-    std::string s;
-};
-
-std::istream& operator>>(std::istream& in, Line& line)
-{
-    std::getline(in, line.s);
-    return in;
-}
-
-std::ostream& operator<<(std::ostream& out, const Line& line)
-{
-    out << line.s;
-    return out;
-}
-
 void print_view(std::string_view name, std::ranges::viewable_range auto view)
 {
     bool found = false;
@@ -30,12 +14,18 @@ void print_view(std::string_view name, std::ranges::viewable_range auto view)
     for (const auto& item: view) {
         found = true;
         if constexpr (requires { std::cout << item; }) {
-            std::cout << name << ": \"" << item << "\"\n";
+            std::cout << std::format("{}: \"{}\"\n", name, item);
         } else if constexpr (std::ranges::viewable_range<decltype(item)>) {
             print_view(std::format("{}[{}]", name, i), item);
             ++i;
         } else {
+#if 0
             std::cout << "I can't output that!\n";
+#else
+            // A var-arg template can << a tuple
+            auto [a, b] = item;
+            std::cout << std::format("{}: <\"{}\", \"{}\">\n", name, a, b);
+#endif
         }
     }
     if (not found) std::cout << name << " is empty\n";
@@ -43,16 +33,47 @@ void print_view(std::string_view name, std::ranges::viewable_range auto view)
 
 int main()
 {
-    std::istringstream in{"this\nis\na\ntest"};
+    std::istringstream in{
+        "There will be a test.\n"
+            "This is a test.\n"
+            "That was a test.\n"
+    };
     in.unsetf(std::ios_base::skipws);
-    auto raw = std::ranges::istream_view<Line>(in);
-    print_view("raw", raw);
+
+    auto raw = std::ranges::istream_view<char>(in);
 #if 0
+    print_view("raw", raw);
+#endif
+
+    // Views::split requires a forward_range; istream_view is an input_range.
+    // Views::lazy_split can take an input_range.
     auto split = raw | std::views::lazy_split('\n');
-    //Why is split empty?
-    //It works if I don't do it as a separate step.
-    //It works if I don't print raw.
+    // If raw was printed above, split will be empty.
+    // If raw wasn't printed above, all is well.
+#if 0
     print_view("split", split);
 #endif
+
+    auto strings = split | std::views::transform([](auto range)
+            {
+            std::ranges::common_view cv(range);
+            return std::string(std::ranges::begin(cv), std::ranges::end(cv));
+            });
+#if 0
+    print_view("strings", strings);
+#endif
+
+    auto enumerated = strings | std::views::enumerate;
+#if 0
+    print_view("enumerated", enumerated);
+#endif
+
+    // The strings are lost...even if we never called print_view.
+    auto filtered = enumerated | std::views::filter([](auto tuple)
+            {
+            auto [number, text] = tuple;
+            return number % 2;
+            });
+    print_view("filtered", filtered);
 }
 
