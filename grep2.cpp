459d3c5692ff77b3214548321450f63748bed8cb
Robert Fisher <robertfi@usau-robertfi-m.attlocal.net>
2023-07-27 14:35:17 -0500

    Deleted old lines from grep2.cpp

--- grep2.cpp
+++ grep2.cpp
@@ -82,37 +82,7 @@ int main(const int argc, const char** argv)
             });
     r::copy(output,
             s::ostream_iterator<s::string>(s::cout, "\n"));
-#if 0
-    // If a file name is given, open it; otherwise, use stdin.
-    s::ifstream fin;
-    if (args.size() > 1) {
-        // Can't pass a string_view, so convert it to a string.
-        fin.open(s::string{args[1]});
-    }
-    s::istream& in = fin.is_open()? fin: s::cin;
 
-    // A helper function for the filter view below.
-    auto match = [&pattern](const auto& tuple)
-    {
-        const auto& [number, line] = tuple;
-        return s::regex_search(line, pattern);
-    };
-
-    // Turn an <int, string> tuple-like object into a string:
-    auto stringify = [](const auto& tuple)
-    {
-        const auto& [number, line] = tuple;
-        return s::format("{}: {}", number + 1, line);
-    };
-
-    // Do the work and write it to stdout:
-    r::copy(r::istream_view<Line>(in)   // Read lines
-            | v::transform(&Line::text) // Turn line objects into strings
-            | v::enumerate              // Number the lines
-            | v::filter(match)          // Do the regex match
-            | v::transform(stringify),  // Format the output
-            s::ostream_iterator<s::string>(s::cout, "\n"));
-#endif
     return EXIT_SUCCESS;
 }
 
